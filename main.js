226073cf0c0249b3bbeba3c7e4343dc2642bd704
Mushrif Ali Khan <mushrifali@gmail.com>
2025-02-06 23:38:43 +0530

    Download Started

--- main.js
+++ main.js
@@ -1,42 +1,216 @@
-const {app, BrowserWindow} = require('electron')
-    const url = require("url");
-    const path = require("path");
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, dialog} = require('electron')
+// const {download} = require('electron-dl');
+const path = require('path')
+const fs = require('fs');
+const { autoUpdater } = require('electron-updater')
+const log = require("electron-log");
+const url = require("url");
+const server = 'https://www.accountancyapp.in'
 
-    let mainWindow
+let mainWindow
 
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
-
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/accountancy-app/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-
-      // Open the DevTools.
-      //   mainWindow.webContents.openDevTools()
-
-      mainWindow.maximize()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
     }
+  })
 
-    app.on('ready', createWindow)
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/accountancy-app/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
 
-    app.on('window-all-closed', function () {
+  // Open the DevTools.
+  //   mainWindow.webContents.openDevTools()
+
+  mainWindow.maximize()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  // e.g. for Windows and app version 1.2.3
+  // https://your-deployment-url.com/update/win32/1.2.3
+  // const feedURL = `${server}/update/${process.platform}/${app.getVersion()}`
+  const feedURL = `${server}/update`
+
+  console.log(feedURL);
+  log.info(feedURL);
+
+  autoUpdater.forceDevUpdateConfig = true;
+  autoUpdater.setFeedURL(feedURL)
+  autoUpdater.checkForUpdates();
+}
+
+app.on('ready', createWindow)
+
+app.on('ready', function()  {
+  // console.log(process);
+  // DownloadSetup();
+  autoUpdater.logger = log;
+  autoUpdater.logger.transports.file.level = "info"
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
+    // In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+global.downloadPath = path.join( app.getPath( "downloads" ), "accountancy-app" );
+if ( !fs.existsSync( global.downloadPath ) ) fs.mkdirSync( global.downloadPath );
+
+log.info(global.downloadPath);
+
+function isNetworkError(errorObject) {
+  return errorObject.message === "net::ERR_INTERNET_DISCONNECTED" ||
+      errorObject.message === "net::ERR_PROXY_CONNECTION_FAILED" ||
+      errorObject.message === "net::ERR_CONNECTION_RESET" ||
+      errorObject.message === "net::ERR_CONNECTION_CLOSE" ||
+      errorObject.message === "net::ERR_NAME_NOT_RESOLVED" ||
+      errorObject.message === "net::ERR_CONNECTION_TIMED_OUT";
+}
+
+function DownloadSetup(version){
+
+  // console.log(process);
+  let packageUrl = `${server}/update/${process.arch}/accountancy-app-${version}-setup.zip`;
+  download( mainWindow, packageUrl, {
+    directory: global.downloadPath,
+    onStarted: item => {
+      console.log(item);
+      console.log('download starts...');
+      mainWindow.webContents.send('downloadStart', null);
+    },
+    onProgress: progress => {
+      let log_message = progress.percent + "% | " + progress.transferredBytes + " bytes out of " + progress.totalBytes + " bytes.";
+      log.info(log_message);
+      setBar(progress.percent);
+    },
+    onCancel: item => {
+      console.log('download canceled.');
+      console.log(item);
+    },
+    openFolderWhenDone: true
+  }).then(() => {
+    onDownloadComplete();
+  });
+
+}
+
+function setBar(percentage) {
+  mainWindow.webContents.send('progress', percentage);
+}
+
+function onDownloadComplete() {
+  mainWindow.webContents.send('downloadComplete', null);
+  
+  dialog.showMessageBox({
+    title: 'Install Updates',
+    message: 'Updates downloaded, application will be quit for update...'
+  }).then(() => {
+    setImmediate(() => {
       if (process.platform !== 'darwin') app.quit()
-    })
+    });
+  });
+}
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-    })
+// setInterval(() => {
+//   autoUpdater.checkForUpdates()
+// }, 60000)
+
+
+let updater
+autoUpdater.autoDownload = true
+
+// autoUpdater.checkForUpdates();
+
+
+autoUpdater.on('error', (error) => {
+  console.error("Updater Error:", error);
+  if (isNetworkError(error)) {
+      log.info('Network Error');
+  } else {
+    dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+  }
+})
+
+// autoUpdater.on('update-available', (updateInfo) => {
+//   console.log('update-available');
+//   dialog.showMessageBox({
+//     type: 'info',
+//     title: 'Found Updates',
+//     message: 'Found updates, do you want update now?',
+//     buttons: ['Sure', 'No']
+//   }).then((res) => {
+//     if (res.response === 0) {
+//       DownloadSetup(updateInfo.version);
+//     }
+//     else {
+//       updater = null
+//     }
+//   });
+// })
+
+// Auto-updater events
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = `Download speed: ${progressObj.bytesPerSecond}`;
+  log_message += ` - Downloaded ${progressObj.percent.toFixed(2)}%`;
+  log_message += ` (${progressObj.transferred}/${progressObj.total})`;
+
+  console.log(log_message);
+
+  // Send progress to renderer process
+  mainWindow?.webContents.send('update-progress', progressObj.percent);
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+autoUpdater.on('update-not-available', () => {
+  dialog.showMessageBox({
+    title: 'No Updates',
+    message: 'Current version is up-to-date.'
+  })
+  // updater.enabled = true
+  updater = null
+})
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    title: 'Install Updates',
+    message: 'Updates downloaded, application will be quit for update...'
+  }, () => {
+    setImmediate(() => autoUpdater.quitAndInstall())
+  });
+})
+  
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+})
+
+function sendStatusToWindow(text) {
+    log.info(text);
+    mainWindow.webContents.send( "message", text )
+    // homePageWindow.webContents.send('message', text);
+}
---
+++ renderer.js
@@ -0,0 +1,49 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const ipcRenderer = require('electron').ipcRenderer
+
+ipcRenderer.on('downloadStart', function (event) {
+    // document.getElementById("bar").style.display = "block";
+});
+
+ipcRenderer.on('progress', function (event, percentage) {
+    // console.log(percentage);
+    // document.getElementById("bar").style.width = (percentage * 100) + "%";
+    // document.getElementById("bar").innerHTML = "Downloading " + parseInt(percentage * 100) + "%";
+});
+
+ipcRenderer.on('downloadComplete', function (event) {
+    // document.getElementById("bar").style.display = "none";
+});
+
+ipcRenderer.on('store-data', function (event,store) {
+    // console.log(store);
+});
+
+ipcRenderer.on('update_available', () => {
+    console.log('Update available. Downloading...');
+    // Notify the user
+});
+
+ipcRenderer.on('update_downloaded', () => {
+    console.log('Update downloaded. Restart to apply.');
+    // Prompt the user to restart
+    const result = confirm('A new update is ready. Restart the app to apply it?');
+    if (result) {
+        ipcRenderer.send('restart_app');
+    }
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
+
+
+function isOnline() {
+    return navigator.onLine;
+}
