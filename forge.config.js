e00fb52374595eda0f8dfb5587725d06ea160093
Mushrif Ali Khan <mushrifali@gmail.com>
2025-02-12 17:58:43 +0530

    updated forge.config

--- forge.config.js
+++ forge.config.js
@@ -3,6 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
+    name: "AccountancyApp",
     asar: true,
     icon: "assets/icon",
     extraResource: [
